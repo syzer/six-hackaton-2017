42026549616b931274d312670663a4b0ac6de31b
syzer3@gmail.com <syzer3@gmail.com>
2017-03-11 03:18:13 +0100

    onError

--- server/api/userReviews/index.js
+++ server/api/userReviews/index.js
@@ -2,13 +2,16 @@ const axios = require('axios')
 const { protocol, ip, port } = require('../../config')
 const baseUrl = `${protocol}${ip}:${port}`
 
+const onError = (next) =>
+    (error) => {
+        console.log(error)
+        next(error)
+    }
+
 const getReviews = (req, res, next) =>
     axios.get(baseUrl + '/products/1')
         .then(({ data }) => data)
         .then(product => res.json(product))
-        .catch((error) => {
-            console.log(error)
-            next(error)
-        })
+        .catch(onError(next))
 
 module.exports = getReviews
