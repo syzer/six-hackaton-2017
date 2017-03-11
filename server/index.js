19162ead108afe2530aab6f40578abd007fe23bc
syzer3@gmail.com <syzer3@gmail.com>
2017-03-11 01:35:37 +0100

    config, and cors, please npm install

--- server/index.js
+++ server/index.js
@@ -1,16 +1,21 @@
 // server.js
 const fs = require('fs')
 const jsonServer = require('json-server')
+// const users = require('./db/users')
+const { port, ip } = require('./config')
+const cors = require('cors')
+
 const server = jsonServer.create()
 const router = jsonServer.router('./db/db.json')
-const users = require('./db/users')
 const middlewares = jsonServer.defaults()
-server.use('/users', (req, res, next) => {
-    console.log(req)
-    res.json(users())
-})
+
+server.use(cors())
+// server.use('/users', (req, res, next) => {
+//     console.log(req)
+//     res.json(users())
+// })
 server.use(middlewares)
 server.use(router)
-server.listen(3000, () => {
-  console.log('JSON Server is running on http://localhost:3000/')
+server.listen(port, ip, () => {
+    console.log(`JSON Server is running on http://${ip}:${port}/`)
 })
