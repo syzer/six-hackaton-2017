9f114e736a073a04851913fe666f033f70d05db1
syzer3@gmail.com <syzer3@gmail.com>
2017-03-11 02:34:24 +0100

    assets served

--- server/index.js
+++ server/index.js
@@ -1,21 +1,20 @@
-// server.js
 const fs = require('fs')
+const path = require('path')
 const jsonServer = require('json-server')
 // const users = require('./db/users')
 const { port, ip } = require('./config')
 const cors = require('cors')
+const express = require('express')
 
 const server = jsonServer.create()
 const router = jsonServer.router('./db/db.json')
 const middlewares = jsonServer.defaults()
 
 server.use(cors())
-// server.use('/users', (req, res, next) => {
-//     console.log(req)
-//     res.json(users())
-// })
 server.use(middlewares)
-server.use(router)
+server.use('/assets', express.static(path.join(__dirname, 'assets')))
+server.use('/', router)
+
 server.listen(port, ip, () => {
     console.log(`JSON Server is running on http://${ip}:${port}/`)
 })
