69c12be02ba08b2b6b0cbf81fad7c438ee95d328
syzer3@gmail.com <syzer3@gmail.com>
2017-03-11 03:02:44 +0100

    get product 1

--- server/api/userReviews/index.js
+++ server/api/userReviews/index.js
@@ -1,7 +1,14 @@
-const getReviews = (req, res) => {
-    console.log('bllaa')
+const axios = require('axios')
+const { protocol, ip, port } = require('../../config')
+const baseUrl = `${protocol}${ip}:${port}`
 
-    res.json({ id: 1, value: 'reviews' })
-}
+const getReviews = (req, res, next) =>
+    axios.get(baseUrl + '/products/1')
+        .then(({ data }) => data)
+        .then(product => res.json(product))
+        .catch((error) => {
+            console.log(error)
+            next(error)
+        })
 
 module.exports = getReviews
--- server/config/index.js
+++ server/config/index.js
@@ -1,4 +1,5 @@
 module.exports = {
+    protocol: 'http://',
     ip: process.env.ip || '0.0.0.0',
     port: process.env.port || 3000
 }
